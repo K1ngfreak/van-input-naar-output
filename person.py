e72ac18521742e129b66f3deddd7cf19dc3e5966
Kingfreak <noahzonneveld@hotmail.com>
2021-09-08 13:20:41 +0200

    address

---
+++ person.py
@@ -0,0 +1,9 @@
+name = input('Naam: ')
+address = input('Adres + huisnummer: ')
+postcode = input('Postcode: ')
+woonplaats = input('Woonplaats: ')
+
+print('Naam:        ' + name)
+print('Adres:       ' + address)
+print('Postcode:    ' + postcode)
+print('Woonplaats:  ' + woonplaats)
